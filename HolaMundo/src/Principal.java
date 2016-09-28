b7ec092472cd52bed10e6269d0c4caa04d150ce3
alumno <alumno@w701>
2016-09-28 09:23:49 +0200

    He duplicado la linea principal

--- HolaMundo/src/Principal.java
+++ HolaMundo/src/Principal.java
@@ -3,6 +3,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo.");
+		System.out.println("Hola mundo.");
+		System.out.println("Hola mundo.");
+		System.out.println("Hola mundo.");
+		System.out.println("Hola mundo.");
+		System.out.println("Hola mundo.");
+		System.out.println("Hola mundo.");
+		System.out.println("Hola mundo.");
 
 	}
 
